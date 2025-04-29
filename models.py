3a6e476801bba47716ab2840ce579f38936a2d61
ervin210 <40193100-ervin210@users.noreply.replit.com>
2025-04-29 13:49:03 +0000

    Restored to 'b4aa7814701dda182a034c06c0fd19f0564d43bc'
    Replit-Restored-To: b4aa7814701dda182a034c06c0fd19f0564d43bc

--- models.py
+++
@@ -1,7 +0,0 @@
-# This file would normally contain database models, but since we're
-# not using a database for this application, it's essentially empty.
-# If expanded in the future, we might add models for:
-# - Command history
-# - User accounts
-# - Saved commands
-# - Community contributions
